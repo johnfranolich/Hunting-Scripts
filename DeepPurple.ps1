6a6763b6ab43aa8ab048cac25942fb08188690f8
johnfranolich <john.franolich@gmail.com>
2018-03-11 20:04:50 -0400

    Add files via upload

---
+++ DeepPurple.ps1
@@ -0,0 +1,49 @@
+﻿<#
+DeepPurple.ps1
+--------------
+
+John "Fran the Man" Franolich
+and his humble sidekick
+
+Note: Keep this in the same directory as:
+    DeepBlue.ps1
+    whitelist.txt
+    regex.txt
+
+#>
+
+
+
+param ([string]$file=$env:file,[string]$log=$env:log)   
+
+function ProcessEventsDirDeepDive([string]$file) 
+{ 
+   
+    # Deepblue on the top-level path
+    $dirpath = $file + "\*"
+    .\DeepBlue $dirpath
+
+    
+    foreach ($item in Get-ChildItem $file)
+    {
+        if ($exclude | Where {$item -like $_}) { continue }
+
+        if (Test-Path $item.FullName -PathType Container) 
+        {
+            ProcessEventsDirDeepDive($item.FullName)
+        } 
+    } 
+    
+    
+}
+      
+
+function Main 
+{   
+    ProcessEventsDirDeepDive($file) 
+}
+
+. Main
+
+
+
